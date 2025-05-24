98534210c8107f3d3b3ca5efecc383f26dc54d94
Hozaifah47 <md.hozaifah1992008@gmail.com>
2025-05-25 00:05:09 +0530

    commit

--- CombSort.java
+++ CombSort.java
@@ -3,7 +3,8 @@ import java.util.*;
 class CombSort {
     public static void main(String[] args) {
 
-        int size, i;
+        int size, gap, i;
+        boolean check = true;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array :");
         size = sc.nextInt();
@@ -11,6 +12,26 @@ class CombSort {
         for (i = 0; i < size; i++) {
             System.out.println("Enter a number (count: " + (i + 1) + ")");
             arr[i] = sc.nextInt();
+        }
+        gap = size;
+
+        while (gap > 1 || check) {
+            gap = (int) (gap / 1.3);
+            if (gap < 1) {
+                gap = 1;
+            }
+
+            check = false;
+            for (i = 0; i + gap < arr.length; i++) {
+                if (arr[i] > arr[i + gap]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + gap];
+                    arr[i + gap] = temp;
+                    check = true;
+                }
+
+            }
+
         }
 
     }
