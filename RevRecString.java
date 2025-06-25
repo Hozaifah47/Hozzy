fae46ae41c258dec7f2304802cb685acfcf9ea20
Hozaifah47 <md.hozaifah1992008@gmail.com>
2025-06-26 00:38:57 +0530

    commit

--- RevRecString.java
+++ RevRecString.java
@@ -4,12 +4,19 @@ public class RevRecString {
     public static void main(String[] args) {
 
         rev ob = new rev();
-
+        Exponents jk = new Exponents();
+        Binary dc = new Binary();
         String str;
+        int a, b;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence");
         str = sc.nextLine();
         System.out.println(ob.getReverse(str));
+        System.out.println("enter a ,b");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        jk.getExponents(a, b);
+        System.out.println(dc.getBinary(a));
 
     }
 
@@ -24,4 +31,24 @@ class rev {
         return getReverse(str.substring(1)) + str.charAt(0);
     }
 
+}
+
+class Exponents {
+    int getExponents(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return a * getExponents(a, b - 1);
+    }
+}
+// gcd of 2 num deci bi find max replace a with x in a string remove dupes from
+// string print or subsets of a string sum of elements in an array
+
+class Binary {
+    String getBinary(int n) {
+        if (n == 0) {
+            return "";
+        }
+        return getBinary(n / 2) + (n % 2);
+    }
 }
